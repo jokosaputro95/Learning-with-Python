5f3a288aad56fba6abf27b76d3e805c0dba2ded8
Joko Saputro <jokosaputro616@gmail.com>
2022-12-21 23:40:36 +0700

    menambahkan file Hello.py

---
+++ Source/Python-Basic/Hello.py
@@ -0,0 +1,10 @@
+my_name = "Joko Saputro"
+print("Hello and welcome " + my_name + "!")
+print(f"Hello and welcome {my_name}!") # * ini formating String
+
+# Ini adalah Comments, teks yang ditulis dalam program tetapi tidak akan dijalankan oleh komputer
+# Python akan menafsirkan apapun setelah tanda # sebagai komentar.
+# ini merupakan bentuk dari single comments
+"""
+ini merupakan bentuk dari multi comments
+"""
